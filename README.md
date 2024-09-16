b9d847ef1511bfa58bc97c9fe7fa684c4e1dc38c
neustaiv <neusta@neusta.me>
2024-09-16 20:15:56 +0200

    Server creation, structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tableau de Bord Interactif
+Un tableau de bord pour gérer une boutique en ligne avec visualisation des statistiques de ventes, gestion des produits, et plus.
+
+## Installation
+1. Clonez le dépôt : `git clone https://github.com/ton-utilisateur/mon-tableau-de-bord.git`
+2. Installez les dépendances : `npm install`
+
+## Scripts
+- `npm run build:css` : Compiler Tailwind pour le développement.
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`📞 Le serveur écoute sur le port ${port}`);
+    console.log(`API URL: ${process.env.API_URL}`);
+})
+
+//Route pour /api
+app.get('/api', (req, res) => {
+    res.json({ message: 'Bienvenue dans l\'API !'})
+});
